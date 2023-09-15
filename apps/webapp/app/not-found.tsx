79929c19b5130ce658476e4a5b1d747efc2f7622
Vadim <eugene.s@zpoken.ai>
2023-09-15 16:13:24 +0300

    change not found

--- apps/webapp/app/not-found.tsx
+++ apps/webapp/app/not-found.tsx
@@ -1,18 +1,27 @@
 'use client';
 import Link from 'next/link';
-import { FadeTransition } from '../components';
 import { buttonVariants } from 'ui';
+import { Metadata } from 'next';
+import { generalMetadata } from '../utils';
+import { FadeTransition } from '../components';
+
+export const metadata: Metadata = {
+  title: 'AssetDesk | 404',
+  ...generalMetadata,
+};
 
 export default function NotFound() {
   return (
-    <FadeTransition>
-      <div className='mt-10 flex flex-col'>
-        <h2 className='h1'>Not Found</h2>
-        <p className='h2'>Could not find requested resource</p>
-        <Link href='/' className={buttonVariants()}>
-          Return Home
-        </Link>
-      </div>
-    </FadeTransition>
+    <div className='flex h-full w-[100vw] items-center justify-center'>
+      <FadeTransition>
+        <div className='mt-10 flex flex-col items-center justify-center gap-2'>
+          <h2 className='h1'>Not Found</h2>
+          <p className='h2'>Could not find requested resource</p>
+          <Link href='/' className={buttonVariants()}>
+            Return Home
+          </Link>
+        </div>
+      </FadeTransition>
+    </div>
   );
 }
