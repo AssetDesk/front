b768d74dc3db940073153b17a45f9a1ceaf7c399
Vasylyk Sergiy <vasylyk29@gmail.com>
2023-11-08 19:01:24 +0200

    added new tests for formatNumber

--- apps/webapp/utils/formattedNumber.test.ts
+++ apps/webapp/utils/formattedNumber.test.ts
@@ -5,12 +5,28 @@ describe('formattedNumber', () => {
   it('returns the correctly formatted number with a variable number of decimal places', () => {
     const num = 1234.5678;
     const digits = 2;
-    expect(formattedNumber(num, digits)).toBe('1.23k');
+    expect(formattedNumber(num, digits)).toBe('1.23K');
   });
 
-  it('returns a correctly formatted number without a variable number of decimal places', () => {
-    const num = 1234;
-    expect(formattedNumber(num)).toBe('1.234k');
+  it('returns 100', () => {
+    const num = 100;
+    expect(formattedNumber(num)).toBe('100');
+  });
+  it('returns 12k', () => {
+    const num = 12000;
+    expect(formattedNumber(num)).toBe('12K');
+  });
+  it('returns 2M', () => {
+    const num = 2000000;
+    expect(formattedNumber(num)).toBe('2M');
+  });
+  it('returns 2.3M', () => {
+    const num = 2300000;
+    expect(formattedNumber(num)).toBe('2.3M');
+  });
+  it('returns 6B', () => {
+    const num = 6000000000;
+    expect(formattedNumber(num)).toBe('6B');
   });
 
   it('returns "0" for a zero input number', () => {
