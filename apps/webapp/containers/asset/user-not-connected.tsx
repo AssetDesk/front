398faad60a28ee03194ad5d827f034a182e822ee
Vadim <eugene.s@zpoken.ai>
2023-11-10 14:09:40 +0200

    webp to svg

--- apps/webapp/containers/asset/user-not-connected.tsx
+++ apps/webapp/containers/asset/user-not-connected.tsx
@@ -9,11 +9,10 @@ export const UserNotConnected = () => {
       <p className='h2'>Your info</p>
       <div className='card-gradient flex flex-col items-center justify-center rounded-lg px-4 pb-10 pt-4  md:px-[30px] md:pb-20 md:pt-14'>
         <Image
-          src='/user-not-connected.webp'
+          src='/user-not-connected.svg'
           alt='user not connected img'
           width={167}
           height={167}
-          priority
         />
         <p className='subtitle1 mb-2 mt-4 text-center'>Please, connect your wallet</p>
         <p className='subtitle2 mb-6 text-center text-[#B0A8A8]'>
